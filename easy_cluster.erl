6b028740049aecc6bb4c986147e8f2f4ea9f3f90
Chad Seaman <chadillac83@gmail.com>
2013-07-11 11:30:03 -0700

    updated plugin to support multimaster clustering

--- easy_cluster.erl
+++ easy_cluster.erl
@@ -1,19 +1,34 @@
 -module(easy_cluster).
 
--export([test_node/1,join/1]).
+-export([test_node/1,join/1,join_as_master/1,sync_node/1]).
 
-test_node(MasterNode) ->
-	case net_adm:ping(MasterNode) of 'pong' ->
-		io:format("server is reachable.~n");
-	_ ->
-		io:format("server could NOT be reached.~n")
-	end.
+test_node(NodeName) ->
+        case net_adm:ping(NodeName) of 'pong' ->
+                io:format("server is reachable.~n");
+        _ ->
+                io:format("server could NOT be reached.~n")
+        end.
 
-join(MasterNode) ->
-	application:stop(ejabberd),
-	mnesia:stop(),
-	mnesia:delete_schema([node()]),
-	mnesia:start(),
-	mnesia:change_config(extra_db_nodes, [MasterNode]),
-	mnesia:change_table_copy_type(schema, node(), disc_copies),
-	application:start(ejabberd).
+join(NodeName) ->
+        mnesia:stop(),
+        mnesia:delete_schema([node()]),
+        mnesia:start(),
+        mnesia:change_config(extra_db_nodes, [NodeName]),
+        mnesia:change_table_copy_type(schema, node(), disc_copies),
+        application:stop(ejabberd),
+        application:start(ejabberd).
+
+join_as_master(NodeName) ->
+        mnesia:stop(),
+        mnesia:delete_schema([node()]),
+        mnesia:start(),
+        mnesia:change_config(extra_db_nodes, [NodeName]),
+        mnesia:change_table_copy_type(schema, node(), disc_copies),
+        easy_cluster:sync_node(NodeName),
+        application:stop(ejabberd),
+        application:start(ejabberd).
+
+sync_node(NodeName) ->
+        [{Tb, mnesia:add_table_copy(Tb, node(), Type)} 
+        || {Tb, [{NodeName, Type}]} <- [{T, mnesia:table_info(T, where_to_commit)}
+        || T <- mnesia:system_info(tables)]].
